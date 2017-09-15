34323a830ea399e703d49072b095753775b453e7
FrecciaRosso <hannochkaherasimuk@gmail.com>
2017-09-15 21:21:36 +0300

    Update src/guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.lowerNumber = 0;
+    	this.greaterNumber = 0;
+    }
 
     setRange(min, max) {
-
+    	this.lowerNumber = min;
+    	this.greaterNumber = max;
     }
 
     guess() {
-
+    	return Math.ceil((this.lowerNumber + this.greaterNumber)/2);
     }
 
     lower() {
-
+    	this.greaterNumber = this.guess();
     }
 
     greater() {
-
+    	this.lowerNumber = this.guess();
     }
 }
 
